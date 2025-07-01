eac1347ed064015789dc60e29f0eacb225f48b07
JEdits20 <jaanerik.sieverding@gmail.com>
2025-07-02 01:08:05 +0200

    init commit

---
+++ android/app/src/main/kotlin/de/jedits/hide_and_seek_city_edition/MainActivity.kt
@@ -0,0 +1,5 @@
+package de.jedits.hide_and_seek_city_edition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'game/menu.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OpenStreetMap Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MapScreen(), // Set MapScreen as the home widget
+    );
+  }
+}
